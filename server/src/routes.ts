7856e9fc75b90db42e15c7c4d12aab3139008be8
Luan Rodrigues <luanlrs96@gmail.com>
2020-06-03 15:51:13 -0300

    Configuração do cadastro de items, mas sem custoimzar o upload de imagens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,37 @@
 import express from 'express'
-
+import knex from './database/connection'
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-    return res.json({message: "Hello World"})
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*')
+    const seralizedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.titulo,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    })
+    return res.json(seralizedItems)
+})
+
+routes.post('/points', async (req, res) => {
+    const { name, email, whatsapp, latitude, longitude, city, uf, items } = req.body;
+
+    const trx = await knex.transaction()
+    const insertIds = await trx('points').insert({
+        image: 'image-fake', name, email, whatsapp, latitude, longitude, city, uf, 
+    })
+
+    const point_id = insertIds[0]
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        }
+    })
+    await trx('point_items').insert(pointItems)
+    return res.json({success: true})
 })
 
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express, { response } from 'express';
+import path from 'path'
 import routes from './routes';
 
 
@@ -6,6 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 // app.get('/users', (req, res) => {
 //     const search = String(req.query.search);
